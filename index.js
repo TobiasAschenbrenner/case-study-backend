3e772ef5cbae9dada6ca78e360959ecbecc5b18f
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-14 18:23:07 +0100

    🗃️ Integrate MongoDB with Mongoose and add dotenv for environment variables

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.use("/", (req, res) => {
-  res.send("Hello World!");
-});
+// DB Connection
+const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected!");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.listen(8800, () => {
+  connectMongoDB();
   console.log("Backend server is running!");
 });
