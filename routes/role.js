58cdc729180bcc3e7905ba3c43e0cb783991a112
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-11-15 18:19:00 +0100

    🔒️ Add admin verification to all role management routes

--- routes/role.js
+++ routes/role.js
@@ -5,19 +5,20 @@ import {
   updateRole,
   deleteRole,
 } from "../controllers/role.controller.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // Create a new role in DB
-router.post("/create", createRole);
+router.post("/create", verifyAdmin, createRole);
 
 // Update a role in DB
-router.put("/update/:id", updateRole);
+router.put("/update/:id", verifyAdmin, updateRole);
 
 // Get all roles from DB
-router.get("/getAll", getAllRoles);
+router.get("/getAll", verifyAdmin, getAllRoles);
 
 // Delete a role from DB
-router.delete("/deleteRole/:id", deleteRole);
+router.delete("/deleteRole/:id", verifyAdmin, deleteRole);
 
 export default router;
